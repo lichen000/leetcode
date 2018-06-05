86d6af8a717e4506b23dc432eb4f7f096f2fb6dd
chen.li200李晨 <chen.li200@chinaredstar.com>
2018-06-05 19:24:48 +0800

    53

---
+++ src/mangolost/Solution0053.java
@@ -0,0 +1,45 @@
+package mangolost;
+
+//Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+//
+//		Example:
+//
+//		Input: [-2,1,-3,4,-1,2,1,-5,4],
+//		Output: 6
+//		Explanation: [4,-1,2,1] has the largest sum = 6.
+//		Follow up:
+//
+//		If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+/**
+ *
+ */
+public class Solution0053 {
+
+	/**
+	 * @param nums
+	 * @return
+	 */
+	public int maxSubArray(int[] nums) {
+		int len = nums.length;
+		int sum = nums[0];
+		int temp = nums[0];
+		for (int i = 1; i < len; i++) {
+			if (temp > 0) {
+				temp += nums[i];
+			} else {
+				temp = nums[i];
+			}
+			if (sum < temp) {
+				sum = temp;
+			}
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		Solution0053 solution = new Solution0053();
+		int[] nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+		System.out.println(solution.maxSubArray(nums));
+	}
+}
