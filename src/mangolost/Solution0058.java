8f0cf2eaaae30625d755b0a220ca2bb3c4ec34d6
chen.li200李晨 <chen.li200@chinaredstar.com>
2018-06-06 19:18:32 +0800

    58

---
+++ src/mangolost/Solution0058.java
@@ -0,0 +1,45 @@
+package mangolost;
+
+//Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+//
+//		If the last word does not exist, return 0.
+//
+//		Note: A word is defined as a character sequence consists of non-space characters only.
+//
+//		Example:
+//
+//		Input: "Hello World"
+//		Output: 5
+
+/**
+ *
+ */
+public class Solution0058 {
+
+	/**
+	 *
+	 * @param s
+	 * @return
+	 */
+	public int lengthOfLastWord(String s) {
+		if (s == null || s.length() == 0) {
+			return 0;
+		}
+		int len = s.length();
+		int count = 0;
+		for (int i = len - 1; i >= 0; i--) {
+			if (s.charAt(i) != ' ') {
+				count++;
+			} else if (s.charAt(i) == ' ' && count != 0) {
+				return count;
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		Solution0058 solution = new Solution0058();
+		String s = "Hello World";
+		System.out.println(solution.lengthOfLastWord(s));
+	}
+}
