be44fd4a80f06779306f9c04dcccef0d2e83a35f
chen.li200李晨 <chen.li200@chinaredstar.com>
2018-06-15 19:53:22 +0800

    141 160

---
+++ src/mangolost/Solution0141.java
@@ -0,0 +1,81 @@
+package mangolost;
+
+//Given a linked list, determine if it has a cycle in it.
+//
+//		Follow up:
+//		Can you solve it without using extra space?
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ */
+public class Solution0141 {
+
+    /**
+     * Definition for singly-linked list.
+     *
+     */
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+	/**
+	 *
+	 * @param head
+	 * @return
+	 */
+	public boolean hasCycle(ListNode head) {
+		Set<ListNode> visited = new HashSet<>();
+		while (head != null) {
+			if (visited.contains(head)) {
+				return true;
+			} else {
+				visited.add(head);
+				head = head.next;
+			}
+		}
+		return false;
+	}
+
+    /**
+     *
+     * @param arr
+     * @return
+     */
+	private ListNode genListNodes(int[] arr) {
+        ListNode head = new ListNode(arr[0]);
+        ListNode current = head;
+        for (int i = 1, len = arr.length; i < len; i++) {
+            ListNode next = new ListNode(arr[i]);
+            current.next = next;
+            current = next;
+        }
+        if (current.val == 999) {
+        	current.next = head;
+		} else {
+			current.next = null;
+		}
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Solution0141 solution = new Solution0141();
+
+        int[] arr1 = new int[]{1,2,4};
+        ListNode head1 = solution.genListNodes(arr1);
+
+        int[] arr2 = new int[]{1,3,4,999};
+        ListNode head2 = solution.genListNodes(arr2);
+
+        System.out.println(solution.hasCycle(head1));
+		System.out.println(solution.hasCycle(head2));
+
+    }
+}
---
+++ src/mangolost/Solution0160.java
@@ -0,0 +1,88 @@
+package mangolost;
+
+//Write a program to find the node at which the intersection of two singly linked lists begins.
+//
+//
+//		For example, the following two linked lists:
+//
+//		A:          a1 → a2
+//		↘
+//		c1 → c2 → c3
+//		↗
+//		B:     b1 → b2 → b3
+//		begin to intersect at node c1.
+//
+//
+//		Notes:
+//
+//		If the two linked lists have no intersection at all, return null.
+//		The linked lists must retain their original structure after the function returns.
+//		You may assume there are no cycles anywhere in the entire linked structure.
+//		Your code should preferably run in O(n) time and use only O(1) memory.
+
+/**
+ *
+ */
+public class Solution0160 {
+
+    /**
+     * Definition for singly-linked list.
+     *
+     */
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+	/**
+	 *
+	 * @param headA
+	 * @param headB
+	 * @return
+	 */
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		ListNode a = headA, b = headB;
+		while (a != b) {
+			if (a != null) {
+				a = a.next;
+			} else {
+				a = headB;
+			}
+			if (b != null) {
+				b = b.next;
+			} else {
+				b = headA;
+			}
+		}
+		return a;
+	}
+
+    public static void main(String[] args) {
+        Solution0160 solution = new Solution0160();
+
+        ListNode headA = new ListNode(1);
+        ListNode nodeA1 = new ListNode(2);
+
+        ListNode headB = new ListNode(3);
+        ListNode nodeB1 = new ListNode(4);
+        ListNode nodeB2 = new ListNode(5);
+
+        ListNode nodeC1 = new ListNode(6);
+        ListNode nodeC2 = new ListNode(7);
+
+        headA.next = nodeA1;
+        nodeA1.next = nodeC1;
+
+        headB.next = nodeB1;
+        nodeB1.next = nodeB2;
+        nodeB2.next = nodeC1;
+
+        nodeC1.next = nodeC2;
+
+        System.out.println(solution.getIntersectionNode(headA, headB).val);
+    }
+}
