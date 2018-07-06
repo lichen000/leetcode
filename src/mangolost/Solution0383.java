42b853cf09837b4eef4553a4cc76b36a980ae29b
mangolost <lichen0_0@163.com>
2018-07-06 22:18:03 +0800

    383 784

---
+++ src/mangolost/Solution0383.java
@@ -0,0 +1,45 @@
+package mangolost;
+
+//Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+//
+//        Each letter in the magazine string can only be used once in your ransom note.
+//
+//        Note:
+//        You may assume that both strings contain only lowercase letters.
+//
+//        canConstruct("a", "b") -> false
+//        canConstruct("aa", "ab") -> false
+//        canConstruct("aa", "aab") -> true
+
+/**
+ *
+ */
+public class Solution0383 {
+
+    /**
+     * @param ransomNote
+     * @param magazine
+     * @return
+     */
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] times = new int[26];
+        for (char ch : magazine.toCharArray()) {
+            times[ch - 'a']++;
+        }
+        for (char ch : ransomNote.toCharArray()) {
+            if (times[ch - 'a'] > 0) {
+                times[ch - 'a']--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution0383 solution = new Solution0383();
+        System.out.println(solution.canConstruct("a", "b"));
+        System.out.println(solution.canConstruct("aa", "ab"));
+        System.out.println(solution.canConstruct("aa", "aab"));
+    }
+}
---
+++ src/mangolost/Solution0784.java
@@ -0,0 +1,65 @@
+package mangolost;
+
+//Given a string S, we can transform every letter individually to be lowercase or uppercase to create another string.  Return a list of all possible strings we could create.
+//
+//        Examples:
+//        Input: S = "a1b2"
+//        Output: ["a1b2", "a1B2", "A1b2", "A1B2"]
+//
+//        Input: S = "3z4"
+//        Output: ["3z4", "3Z4"]
+//
+//        Input: S = "12345"
+//        Output: ["12345"]
+//        Note:
+//
+//        S will be a string with length at most 12.
+//        S will consist only of letters or digits.
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ */
+public class Solution0784 {
+
+    /**
+     * @param S
+     * @return
+     */
+    public List<String> letterCasePermutation(String S) {
+        LinkedList<String> result = new LinkedList<>();
+        result.add(S);
+        int len = S.length();
+        for (int i = 0; i < len; i++) {
+            char ch = S.charAt(i);
+            if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+                int size = result.size();
+                for (int j = 0; j < size; j++) {
+                    String str = result.poll();
+                    if (str != null) {
+                        result.add(str.substring(0, i) + Character.toUpperCase(ch) + str.substring(i + 1));
+                        result.add(str.substring(0, i) + Character.toLowerCase(ch) + str.substring(i + 1));
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Solution0784 solution = new Solution0784();
+        String s1 = "a1b2", s2 = "3z4", s3 = "12345";
+        List<String> list1 = solution.letterCasePermutation(s1);
+        list1.forEach(System.out::println);
+
+        List<String> list2 = solution.letterCasePermutation(s2);
+        list2.forEach(System.out::println);
+
+        List<String> list3 = solution.letterCasePermutation(s3);
+        list3.forEach(System.out::println);
+
+    }
+}
