8d10ac408348466cd1f2fa424e2b9201a23bf830
mangolost <lichen0_0@163.com>
2018-10-03 22:45:01 +0800

    15 46

---
+++ src/mangolost/Solution0015.java
@@ -0,0 +1,80 @@
+package mangolost;
+
+//Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+//
+//        Note:
+//
+//        The solution set must not contain duplicate triplets.
+//
+//        Example:
+//
+//        Given array nums = [-1, 0, 1, 2, -1, -4],
+//
+//        A solution set is:
+//        [
+//        [-1, 0, 1],
+//        [-1, -1, 2]
+//        ]
+
+import java.util.*;
+
+/**
+ *
+ */
+public class Solution0015 {
+
+    /**
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        int len = nums.length;
+        if (len < 3) {
+            return result;
+        }
+        Arrays.sort(nums);
+        Set<List<Integer>> set = new HashSet<>();
+        for (int i = 0; i < len - 2; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            int low = i + 1;
+            int high = len - 1;
+            while (low < high) {
+                if (nums[low] + nums[high] + nums[i] == 0) {
+                    if (!set.contains(Arrays.asList(nums[i], nums[low], nums[high]))) {
+                        set.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                        result.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                    }
+                    while (low < high && nums[low] == nums[low + 1]) {
+                        low++;
+                    }
+                    while (low < high && nums[high] == nums[high - 1]) {
+                        high--;
+                    }
+                    low++;
+                    high--;
+                } else if (nums[low] + nums[high] + nums[i] > 0) {
+                    high--;
+                } else {
+                    low++;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution0015 solution = new Solution0015();
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> list = solution.threeSum(nums);
+        for (List<Integer> aList : list) {
+            System.out.print("[ ");
+            for (Integer anAList : aList) {
+                System.out.print(anAList + " ");
+            }
+            System.out.println("]");
+        }
+    }
+}
---
+++ src/mangolost/Solution0046.java
@@ -0,0 +1,75 @@
+package mangolost;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//Given a collection of distinct integers, return all possible permutations.
+//
+//        Example:
+//
+//        Input: [1,2,3]
+//        Output:
+//        [
+//        [1,2,3],
+//        [1,3,2],
+//        [2,1,3],
+//        [2,3,1],
+//        [3,1,2],
+//        [3,2,1]
+//        ]
+
+/**
+ *
+ */
+public class Solution0046 {
+
+    /**
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums.length == 0) {
+            //do nothing
+        } else if (nums.length == 1) {
+            List<Integer> sublist = new ArrayList<>();
+            sublist.add(nums[0]);
+            result.add(sublist);
+        } else {
+            List<Integer> list = new ArrayList<>();
+            Arrays.sort(nums);
+            doPermute(nums, list, result);
+        }
+        return result;
+    }
+
+    /**
+     *
+     * @param nums
+     * @param list
+     * @param result
+     */
+    private static void doPermute(int[] nums, List<Integer> list, List<List<Integer>> result) {
+        if (list.size() == nums.length) {
+            result.add(new ArrayList<>(list));
+        } else {
+            for (int num : nums) {
+                if (list.contains(num)) {
+                    continue;
+                }
+                list.add(num);
+                doPermute(nums, list, result);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution0046 solution = new Solution0046();
+        int[] nums = {1, 9, 3, 7, 6};
+        List<List<Integer>> result = solution.permute(nums);
+        System.out.println(result);
+    }
+}
